cb9d42f885815d60b8d6b394da40f5a161aeb160
Slawomir Piwowarski <slawomir.piwowarski@payu.pl>
2016-12-28 22:05:50 +0100

    Setting Bean Properties

--- src/main/java/com/xcafe/spring/test/Person.java
+++ src/main/java/com/xcafe/spring/test/Person.java
@@ -6,22 +6,27 @@ public class Person {
 
     private int id;
     private String name;
+    private int age;
 
     public Person(int id, String name) {
         this.id = id;
         this.name = name;
     }
 
+    public void setAge(int age) {
+        this.age = age;
+    }
+
     public void sayHello() {
         System.out.println("Hello, I am " + name);
     }
-
 
     @Override
     public String toString() {
         return new ToStringBuilder(this)
                 .append("id", id)
                 .append("name", name)
+                .append("age", age)
                 .toString();
     }
 }
