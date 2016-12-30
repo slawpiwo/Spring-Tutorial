f322a7b8234aa31399faaa918430c41cbc7f2e2b
Slawomir Piwowarski <slawomir.piwowarski@payu.pl>
2016-12-30 08:38:08 +0100

    Property Maps

--- src/main/java/com/xcafe/spring/test/Organizer.java
+++ src/main/java/com/xcafe/spring/test/Organizer.java
@@ -2,16 +2,23 @@ package com.xcafe.spring.test;
 
 import org.apache.commons.lang3.builder.ToStringBuilder;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Organizer {
 
     private String name;
     private List<String> tasks;
+    private Map<String, String> notes = new HashMap<String, String>();
 
     public Organizer(String name, List<String> tasks) {
         this.name = name;
         this.tasks = tasks;
+    }
+
+    public void setNotes(Map<String, String> notes) {
+        this.notes = notes;
     }
 
 
@@ -20,6 +27,7 @@ public class Organizer {
         return new ToStringBuilder(this)
                 .append("name", name)
                 .append("tasks", tasks)
+                .append("notes", notes)
                 .toString();
     }
 }
