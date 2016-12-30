dff5970579ae62b927cf04a10264cbca5c6a33a5
Slawomir Piwowarski <slawomir.piwowarski@payu.pl>
2016-12-30 11:02:37 +0100

    Arbitrary Maps as Bean Properties

--- src/main/java/com/xcafe/spring/test/WeekOrganizer.java
+++ src/main/java/com/xcafe/spring/test/WeekOrganizer.java
@@ -2,14 +2,14 @@ package com.xcafe.spring.test;
 
 import org.apache.commons.lang3.builder.ToStringBuilder;
 
-import java.util.List;
+import java.util.Map;
 
 public class WeekOrganizer {
 
     private Person person;
-    private List<Organizer> week;
+    private Map<String, Organizer> week;
 
-    public WeekOrganizer(Person person, List<Organizer> week) {
+    public WeekOrganizer(Person person, Map<String, Organizer> week) {
         this.person = person;
         this.week = week;
     }
